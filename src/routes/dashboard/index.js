9961ad795651593446b270a09a34cf124dbf4454
anthonylin198 <anthony.lin.198@gmail.com>
2021-01-31 17:00:29 -0800

    fixup home

--- src/routes/dashboard/index.js
+++ src/routes/dashboard/index.js
@@ -1,3 +1,40 @@
-import DashboardComponent from './DashboardComponent';
+import React from "react";
 
-export default DashboardComponent;
+import { makeStyles } from "@material-ui/core/styles";
+import { Box, Grid } from "@material-ui/core";
+// import QuickStats from "./QuickStats";
+// import AddCard from "./AddCard";
+// import AssessmentsCard from "./AssessmentsCard";
+// import RecentApplicantsCard from "./RecentApplicantsCard";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+}));
+
+const Dashboard = () => {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      {/* Your Progress */}
+      <Grid container spacing={3}>
+        <Grid item xs={12} md={7}>
+          <h1>here</h1>
+        </Grid>
+        <Grid item xs={12} md={5}>
+          <h1>here</h1>
+        </Grid>
+        {/* Need to make this more responsive, right now just no show on mobile */}
+        <Grid component={Box} item md={7} display={{ xs: "none", md: "block" }}>
+          <h1>here</h1>
+        </Grid>
+        <Grid component={Box} item md={5} display={{ xs: "none", md: "block" }}>
+          <h1>here</h1>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+export default Dashboard;
